0a69e1e59013946b4da0502a8e154c1aafde72fb
m. nathaniel martin <matt.martin@rutgers.edu>
2016-05-07 12:03:31 -0400

    fix connection.js for heroku

--- app/config/connection.js
+++ app/config/connection.js
@@ -7,8 +7,8 @@ var connection = mysql.createConnection({
     port: 3306,
     host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     user: 'e2ucyar9yhpzctcv',
-    password: "z8utudz410ntv3vz"
-    // database: "starwars"
+    password: "z8utudz410ntv3vz",
+    database: "le54gobt3hmb8msp"
 });
     
 // var connection = mysql.createConnection(process.env.JAWSDB_URL);
